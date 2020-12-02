dbd37fcb3faf46faf335490a10d1ab788fc5f5a6
Romain Warnan <romain.warnan@insee.fr>
2020-12-02 11:28:33 +0100

    Day01

---
+++ src/main/java/fr/insee/aoc/days/Day01.java
@@ -0,0 +1,44 @@
+package fr.insee.aoc.days;
+
+import fr.insee.aoc.utils.DayException;
+
+import static fr.insee.aoc.utils.Days.*;
+import static java.lang.Math.*;
+
+public class Day01 implements Day {
+
+	@Override
+	public String part1(String input, Object... params) {
+		int[] expenses = arrayOfInt(input);
+		for(int i = 0; i < expenses.length; i ++) {
+			for(int j = 0; j < expenses.length; j ++) {
+				if(i != j) {
+					int a = expenses[i];
+					int b = expenses[j];
+					if(a + b == 2020) return String.valueOf(a * b);
+				}
+			}
+		}
+		throw new DayException();
+	}
+
+	@Override
+	public String part2(String input, Object... params) {
+		int[] expenses = arrayOfInt(input);
+		for(int i = 0; i < expenses.length; i ++) {
+			int a = expenses[i];
+			for(int j = 0; j < expenses.length; j ++) {
+				int b = expenses[j];
+				if(i != j && a + b < 2020) {
+					for(int k = 0; k < expenses.length; k ++) {
+						if(j != k) {
+							int c = expenses[k];
+							if(a + b + c == 2020) return String.valueOf(a * b * c);
+						}
+					}
+				}
+			}
+		}
+		throw new DayException();
+	}
+}
---
+++ src/test/java/fr/insee/aoc/days/Day01Test.java
@@ -0,0 +1,35 @@
+package fr.insee.aoc.days;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Day01Test  {
+    
+	private Day day = new Day01();
+	
+	@Test
+	public void case1_0() {
+		assertEquals("514579", day.part1("src/test/resources/01-0.txt"));
+	}
+
+	@Test
+	public void case2_0() {
+		assertEquals("241861950", day.part2("src/test/resources/01-0.txt"));
+	}
+
+	@Test
+	public void part1() {
+		String answer = day.part1("src/main/resources/01.txt");
+		System.out.printf("%s.1: %s%n", day.getClass().getSimpleName(), answer);
+		assertEquals("355875", answer);
+	}
+
+	@Test
+	public void part2() {
+		String answer = day.part2("src/main/resources/01.txt");
+		System.out.printf("%s.2: %s%n", day.getClass().getSimpleName(), answer);
+		assertEquals("140379120", answer);
+	}
+}
