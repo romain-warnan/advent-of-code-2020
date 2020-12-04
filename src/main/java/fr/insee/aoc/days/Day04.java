7defba845d4c7b4a96b1f34716b96a6318cc8a7a
Romain Warnan <romain.warnan@insee.fr>
2020-12-04 12:39:38 +0100

    Début Day04

---
+++ src/main/java/fr/insee/aoc/days/Day04.java
@@ -0,0 +1,15 @@
+package fr.insee.aoc.days;
+
+import static fr.insee.aoc.utils.Days.*;
+
+public class Day04 implements Day {
+
+	@Override
+	public String part1(String input, Object... params) {
+		return String.valueOf(0);
+	}
+
+	private static class Passport {
+		private String byr, iyr, eyr, hgt, hcl, ecl, pid, cid;
+	}
+}
---
+++ src/test/java/fr/insee/aoc/days/Day04Test.java
@@ -0,0 +1,38 @@
+package fr.insee.aoc.days;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+public class Day04Test  {
+    
+	private Day day = new Day03();
+	
+	@Test
+	public void case1_0() {
+		assertEquals("2", day.part1("src/test/resources/04-0.txt"));
+	}
+
+	@Test
+	@Disabled
+	public void case2_0() {
+		assertEquals("336", day.part2("src/test/resources/04-0.txt"));
+	}
+
+	@Test
+	public void part1() {
+		String answer = day.part1("src/main/resources/04.txt");
+		System.out.printf("%s.1: %s%n", day.getClass().getSimpleName(), answer);
+		assertEquals("193", answer);
+	}
+
+	@Test
+	@Disabled
+	public void part2() {
+		String answer = day.part2("src/main/resources/04.txt");
+		System.out.printf("%s.2: %s%n", day.getClass().getSimpleName(), answer);
+		assertEquals("1355323200", answer);
+	}
+}
